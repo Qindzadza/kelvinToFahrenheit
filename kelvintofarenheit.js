1106cbb72a5fec504764f1b93fd84ebecfdbe511
Qindzadza <mirian.pipia@gmail.com>
2024-09-23 18:21:22 +0400

    kelvintofahren

---
+++ kelvintofarenheit.js
@@ -0,0 +1,10 @@
+console.log(293)
+//create variabel for kelvin
+const kelvin=0;
+//create variavle for celsius
+let celsius=kelvin-273;
+//formula for farenheit
+let farenheit=Math.floor(celsius*(9/5)+32);
+let newton=Math.floor(celsius*(33/100));
+console.log(newton)
+console.log(`The temperature is ${farenheit} degrees Farenheit`)
